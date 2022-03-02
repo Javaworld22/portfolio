93a038f830cdb555037b31dbf1715be60b8e949c
Iheruome Michael <32968231+Javaworld22@users.noreply.github.com>
2022-03-02 17:56:38 +0100

    second commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 // set the view engine to ejs
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 app.disable('etag');
 
