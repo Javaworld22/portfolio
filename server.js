6be15ff91fecbbdf7b047f93975a58abc362f191
Iheruome Michael <32968231+Javaworld22@users.noreply.github.com>
2022-03-02 17:30:34 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+const bodyParser = require ('body-parser')
+var path = require('path')
+var app = express()
+
+const port = process.env.PORT || 3030
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+// set the view engine to ejs
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.disable('etag');
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use('/assets',express.static(path.join(__dirname,'public/assets')))
+app.use('/user',express.static(path.join(__dirname,'public/plugin')))
+app.use('/font',express.static(path.join(__dirname,'public/assets/fonts')))
+
+app.get('/', function(req, res) {
+    res.render('index');
+  });
+
+app.listen(port, ()=>{console.log('Starting the server at port ' +port)})
